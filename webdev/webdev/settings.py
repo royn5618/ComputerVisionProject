bcc5d128760e721645c7734af350e863befe7de5
NRoy <nroy0110@gmail.com>
2020-06-25 15:26:43 +0100

    updating key

--- webdev/webdev/settings.py
+++ webdev/webdev/settings.py
@@ -21,7 +21,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'YOUR-KEY'
+SECRET_KEY = os.environ.get('DJ_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
