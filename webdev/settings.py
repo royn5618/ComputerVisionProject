4400b5c08930c138a84a5a7acba5878eec81f578
NRoy <nroy0110@gmail.com>
2020-06-27 20:15:11 +0100

    Update debug status

--- webdev/settings.py
+++ webdev/settings.py
@@ -25,7 +25,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = os.environ.get('DJ_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
