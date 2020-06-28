cee9e11c1c8cdb6a194710296184e9b50d5639a6
NRoy <nroy0110@gmail.com>
2020-06-28 11:09:19 +0100

    more settings updates

--- webdev/urls.py
+++ webdev/urls.py
@@ -21,6 +21,6 @@ from django.urls import path
 from webapp import views
 
 urlpatterns = [
-    path("admin/"", admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", views.index, name='index')
 ]
