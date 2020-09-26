f2156d29b9084b0db5f7b3b724f299b3654ba75d
Nabanita <nroy0110@gmail.com>
2020-09-26 21:24:17 +0100

    Adding new project work

--- webdev/settings.py
+++ webdev/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJ_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# Need to be True to run localhost
 DEBUG = False
 
 ALLOWED_HOSTS = []
