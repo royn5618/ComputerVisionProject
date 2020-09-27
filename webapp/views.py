961e2b9a67591dc0afd8759ac3261c9a9ad7dda1
Nabanita <nroy0110@gmail.com>
2020-09-27 18:49:41 +0100

    Addtion of my profile

--- webapp/views.py
+++ webapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def cvprojects(request):
+    return render(request, 'cvprojects.html')
--- webdev/urls.py
+++ webdev/urls.py
@@ -23,4 +23,5 @@ from webapp import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.index, name='index'),
+    path("cvprojects/", views.cvprojects, name='cvprojects'),
 ]
